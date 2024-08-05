176216112efdfd25d74b4076e0e2088fc5634ab2
Toast12 <159939124+toast-12@users.noreply.github.com>
2024-08-06 07:03:17 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@google/generative-ai': '/node_modules/@google/generative-ai/dist/index.ts',
+      'base64-js': '/node_modules/base64-js/base64js.min.js' // 필요한 경우 경로 수정
     },
   },
 });
