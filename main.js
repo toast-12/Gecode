ac36b1cfbdfa1987d906bdfb5b17e024849c029d
Toast12 <159939124+toast-12@users.noreply.github.com>
2024-08-06 10:17:08 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from "./node_mod
 import Base64 from './base64-js';
 import MarkdownIt from './markdown-it';
 import { maybeShowApiKeyBanner } from './gemini-api-banner';
-import '../style.css';
 
 // 🔥🔥 FILL THIS OUT FIRST! 🔥🔥
 // Get your Gemini API key by:
